5e034e62ae4a5fee368792f9bab9f5a1db36659a
amour9579 <hyunkyu9579@gmail.com>
2026-04-26 23:31:40 +0900

    Update KeystoneReporter.lua

--- Core/Config/KeystoneReporter.lua
+++ Core/Config/KeystoneReporter.lua
@@ -10,6 +10,20 @@ local function GetKeystoneDB()
     return ns.db.profile.mythicPlusReporter
 end
 
+local function GetReportFields()
+    local db = GetKeystoneDB()
+    db.reportFields = db.reportFields or {}
+    return db.reportFields
+end
+
+local function GetReportFieldValue(key)
+    return GetReportFields()[key] ~= false
+end
+
+local function SetReportFieldValue(key, value)
+    GetReportFields()[key] = value
+end
+
 function ns:CreateKeystoneReporter()
     return {
         type = "group",
@@ -38,6 +52,54 @@ function ns:CreateKeystoneReporter()
                 name = "|cffffff00※ 채팅에서 !돌, !주차, !keys, !weekly 입력 시 자동으로 응답합니다.|r",
                 order = 2,
                 width = "full",
+            },
+            reportFields = {
+                type = "group",
+                name = "!돌 보고 항목",
+                desc = "체크한 항목만 !돌, !keys, !ehf 응답에 포함합니다. 쐐기돌은 항상 보고합니다.",
+                order = 3,
+                inline = true,
+                args = {
+                    showSpec = {
+                        type = "toggle",
+                        name = "전문화",
+                        desc = "보고 메시지에 현재 전문화를 포함합니다.",
+                        order = 1,
+                        width = "half",
+                        get = function()
+                            return GetReportFieldValue("showSpec")
+                        end,
+                        set = function(_, value)
+                            SetReportFieldValue("showSpec", value)
+                        end,
+                    },
+                    showItemLevel = {
+                        type = "toggle",
+                        name = "템렙 / 아이템 레벨",
+                        desc = "보고 메시지에 현재 착용 아이템 레벨을 포함합니다.",
+                        order = 2,
+                        width = "half",
+                        get = function()
+                            return GetReportFieldValue("showItemLevel")
+                        end,
+                        set = function(_, value)
+                            SetReportFieldValue("showItemLevel", value)
+                        end,
+                    },
+                    showScore = {
+                        type = "toggle",
+                        name = "점수",
+                        desc = "보고 메시지에 쐐기 점수를 포함합니다.",
+                        order = 3,
+                        width = "half",
+                        get = function()
+                            return GetReportFieldValue("showScore")
+                        end,
+                        set = function(_, value)
+                            SetReportFieldValue("showScore", value)
+                        end,
+                    },
+                },
             },
         }
     }
